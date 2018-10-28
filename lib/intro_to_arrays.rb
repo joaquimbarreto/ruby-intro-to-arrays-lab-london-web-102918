fe9ce1e546caeaf49134bada8b936437c7b7b8ff
Joaquim Barreto <joaquim@barreto.co>
2018-10-28 17:38:52 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,10 +5,11 @@
     my_two_array = [1, 2]
   end
 
-def indexing
+
 taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 
-taylor_swift[0]
+def first_element
+  my_first_element = taylor_swift[0]
 end
 
 def third_element
