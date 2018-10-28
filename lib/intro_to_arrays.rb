ab1ff213ee7dabfdfd5eecef228a69949cd2d289
Joaquim Barreto <joaquim@barreto.co>
2018-10-28 17:49:59 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -12,12 +12,12 @@ def first_element(my_first_element)
   my_first_element = taylor_swift[0]
 end
 
-def third_element
+def third_element(my_third_element)
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_third_element = taylor_swift[2]
 end
 
-def last_element
+def last_element(my_last_element)
   taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_last_element = taylor_swift[-1]
 end
@@ -25,14 +25,14 @@ end
 
 
    
- def first_element_with_array_methods
+ def first_element_with_array_methods(first_country)
    south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
    first_country = south_east_asia.first
  end 
  
- def last_element_with_array_methods
+ def last_element_with_array_methods(last_country)
    south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-   last_counrty = south_east_asia.last
+   last_country = south_east_asia.last
  end
       
 programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
