fef9e694854ffc977e97a9b4ac29864e54517215
Joaquim Barreto <joaquim@barreto.co>
2018-10-28 17:45:57 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,7 +7,7 @@
 
 taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 
-def first_element
+def first_element(my_first_element)
   my_first_element = taylor_swift[0]
 end
 
